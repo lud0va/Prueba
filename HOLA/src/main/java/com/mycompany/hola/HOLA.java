693a890a99c19796fce138f3b5c07112aad5f615
Luis <Luis@DESKTOP-0FHM706.home>
2022-12-11 02:31:15 +0100

    saSA

--- HOLA/src/main/java/com/mycompany/hola/HOLA.java
+++ HOLA/src/main/java/com/mycompany/hola/HOLA.java
@@ -13,5 +13,6 @@ public class HOLA {
 
     public static void main(String[] args) {
         System.out.println("prueba desde netbeans");
+       System.out.println("Prueba desde eclipse");
     }
 }
